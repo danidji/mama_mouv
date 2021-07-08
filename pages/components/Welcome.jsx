e899d47e12522ac1235f50933e3146bb37c96c48
dany gbelidji <dany.gbelidji@live.fr>
2021-07-08 16:36:19 +0200

    MOD - Contenue de la page d'accueil

--- pages/components/Welcome.jsx
+++ pages/components/Welcome.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Welcome = (props) => {
 
     return (
-        <>
-            <p>Welcome</p>
-        </>
+        <section className="welcome full-screen">
+            <div className="home_title">
+                <h1 className="title_selima">Bienvenue</h1>
+                <h3 className="title_selima">sur</h3>
+                <div className="logo_content">
+                    <div className="logo_draw">
+                        <Image
+                            src="/images/logo_draw.png"
+                            alt="Mama mouv'"
+                            width={310}
+                            height={500}
+                            layout="responsive"
+                        />
+                        <p>Individuel/collectif</p>
+                    </div>
+
+                    <div className="logo_title">
+                        <Image
+                            src="/images/logo_title.png"
+                            alt="Mama mouv'"
+                            width={725}
+                            height={500}
+                            layout="responsive"
+                        />
+                    </div>
+
+                    <div className="logo_draw">
+                        <Image
+                            src="/images/logo_draw2.png"
+                            alt="Mama mouv'"
+                            width={310}
+                            height={500}
+                            layout="responsive"
+                        />
+                        <p>Femmes enceintes</p>
+                    </div>
+
+                </div>
+                <h2 className="sub_title">Coaching - Paris - IDF Est</h2>
+            </div>
+        </section>
     )
 
 
