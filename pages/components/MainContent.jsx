a0814c74af9628232e56c83f71488cb3c66d6cba
dany gbelidji <dany.gbelidji@live.fr>
2021-07-08 22:41:13 +0200

    MOD - mainContent

--- pages/components/MainContent.jsx
+++ pages/components/MainContent.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+//import GSAP 
+import { gsap } from "gsap/dist/gsap.js";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+
+
+if (process.client) {
+    gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+}
 
 const MainContent = (props) => {
 
     return (
-        <>
-            <p>MainContent</p>
-        </>
+        <section className="panels">
+            <div className="panels_container">
+                <div className="panel">
+                    <h1>Panel 1</h1>
+
+                </div>
+                <div className="panel">
+                    <h1>Panel 2</h1>
+
+                </div>
+                <div className="panel">
+                    <h1>Panel 3</h1>
+                </div>
+            </div>
+        </section>
     )
 
 
