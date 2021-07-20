7a037e619f89653bb580fa2557b3518726e16c3b
dany gbelidji <dany.gbelidji@live.fr>
2021-07-20 19:24:57 +0200

    MOD - Structure scroll horizontal

--- pages/components/MainContent.jsx
+++ pages/components/MainContent.jsx
@@ -2,29 +2,56 @@ import React, { useRef, useEffect } from 'react';
 
 //import GSAP 
 import { gsap } from "gsap/dist/gsap.js";
-import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+// import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 
 
-if (process.client) {
-    gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-}
 
 const MainContent = (props) => {
 
+    gsap.registerPlugin(ScrollTrigger);
+
+    const panelsRef = useRef(null);
+    const panelsContainerRef = useRef(null);
+
+    // Animation scroll défilement verticale
+    useEffect(() => {
+        const panels = gsap.utils.toArray(".panel");
+
+        gsap.to(panels, {
+            xPercent: -100 * (panels.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".panels_container",
+                pin: true,
+                start: "top top",
+                scrub: true,
+                snap: {
+                    snapTo: 1 / (panels.length - 1),
+                    duration: { min: 0.1, max: 0.1 }
+                },
+                end: () => "+=" + (panelsContainerRef.current.offsetWidth - innerWidth)
+            }
+        });
+
+    }, [])
+
     return (
-        <section className="panels">
-            <div className="panels_container">
-                <div className="panel">
+        <section className="panels" ref={panelsRef}>
+            <div className="fake_header">
+
+            </div>
+            <div className="panels_container" ref={panelsContainerRef}>
+                <div className="panel test1">
                     <h1>Panel 1</h1>
 
                 </div>
-                <div className="panel">
+                <div className="panel test2" >
                     <h1>Panel 2</h1>
 
                 </div>
-                <div className="panel">
+                <div className="panel test3" >
                     <h1>Panel 3</h1>
                 </div>
             </div>
