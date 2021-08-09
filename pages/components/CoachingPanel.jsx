8f659be68853588296d7b873bff2242e48082515
dany gbelidji <dany.gbelidji@live.fr>
2021-08-09 19:12:50 +0200

    MOD - Panel Coaching - ajout des fenêtres type coaching

--- pages/components/CoachingPanel.jsx
+++ pages/components/CoachingPanel.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 const CoachingPanel = (props) => {
     return (
         <div className="coaching_panel">
-            <h1>Mes Coaching</h1>
-            <div className="coaching_type">Individuel</div>
-            <div className="coaching_type">Collectif</div>
+            <div className="coaching_head">
+                <h1>Mes Coaching</h1>
+            </div>
+            <div className="window_container">
+                <div className="window individual" data-window="individual">
+                    <div className="window_title">Individuel</div>
+                    <img src="images/coach-indiv.jpg" alt="" />
+                </div>
+                <div className="window collective" data-window="collective">
+                    <div className="window_title">Collectif</div>
+                    <img src="images/coach_collectif.jpg" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
