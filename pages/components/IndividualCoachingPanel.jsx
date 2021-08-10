649033417668e3159c849dc007cc5bf5e2f90451
dany gbelidji <dany.gbelidji@live.fr>
2021-08-10 18:32:30 +0200

    MOD - panel coaching individuel

--- pages/components/IndividualCoachingPanel.jsx
+++ pages/components/IndividualCoachingPanel.jsx
@@ -10,6 +10,32 @@ const IndividualCoachingPanel = (props) => {
                     <AiOutlineCloseCircle />
                 </div>
             </div>
+            <div className="modal_content">
+                <div
+                    className="sub_window"
+                    data-window="perso"
+                    onClick={props.open}
+                >
+                    <div className="subWindow_title" data-window="perso" >Personnalisé</div>
+                    <img src="images/coach-sportif-perso2.jpg" data-window="perso" alt="" />
+                </div>
+                <div
+                    className="sub_window"
+                    data-window="pregnant"
+                    onClick={props.open}
+                >
+                    <div className="subWindow_title" data-window="pregnant" >Femmes enceintes</div>
+                    <img src="images/coaching_jo8.png" data-window="pregnant" alt="" />
+                </div>
+                <div
+                    className="sub_window"
+                    data-window="pilates"
+                    onClick={props.open}
+                >
+                    <div className="subWindow_title" data-window="pilates" >Pilates</div>
+                    <img src="images/pilates2.png" data-window="pilates" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
