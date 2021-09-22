0d03cc55d17f4d68d2dc2362b5651885315c9d1d
dany gbelidji <dany.gbelidji@live.fr>
2021-09-22 16:08:59 +0200

    MOD - index.js récupération de la taille de l'écran

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,50 @@
 // import Image from 'next/image'
+import React, { useState, useEffect, useContext } from "react";
 
 import Layout from "../components/Layout";
 import Welcome from "../components/Welcome";
 import MainContent from "../components/MainContent";
 import Contact from "../components/Contact";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import HambergerMenu from "../components/HambergerMenu";
 
+import animContext from "../context/animContext";
+
 export default function Home() {
-  return (
-    <div className="site">
+    const { windowProps, setWindowProps } = useContext(animContext);
 
-      <Layout navbar={<Navbar />} welcome={<Welcome />} mainContent={<MainContent />} contact={<Contact />} hambergerMenu={<HambergerMenu />} />
+    // function getWindowWidth () {
 
-    </div>
-  )
+    // On détecte si on est sur un navigateur ou non (true si nav false si mobile)
+    const isClient = typeof window === "object";
+    // console.log(`getWindowWidth -> isClient`, isClient);
+
+    let objWindowProps = {
+        isNav: isClient ? true : false,
+        width: isClient ? window.innerWidth : undefined,
+    };
+
+    // Mise à jour de la valeur de windowSize à chaque changement de la lardeur de l'écran
+    useEffect(() => {
+        function handleResize() {
+            setWindowProps(objWindowProps);
+        }
+        //Si je ne suis pas sur un navigateur je renvoie null à windowSize
+        if (!isClient) {
+            setWindowProps(objWindowProps);
+        } else {
+            //Sinon je met à jours la taille de l'écran
+            window.addEventListener("resize", handleResize);
+        }
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    console.log("Width ==> ", objWindowProps);
+
+    return (
+        <div className="site">
+            <Layout navbar={<Navbar />} welcome={<Welcome />} mainContent={<MainContent />} contact={<Contact />} hambergerMenu={<HambergerMenu />} />
+        </div>
+    );
 }
